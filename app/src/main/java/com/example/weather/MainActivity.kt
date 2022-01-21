25b1712123f4025be4ba3d80ed096a9a90692daf
Amanda Bressam <amandabressam@gmail.com>
2022-01-21 09:31:50 -0300

    Created intent val to access the Menu Activity

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.weather
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
@@ -14,6 +15,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val menuButton: ImageButton = findViewById(R.id.menu_button)
+        menuButton.setOnClickListener {
+            // Use intent
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
 
         // Call the function to print the celsius temperature on the screen when the app starts
         convertToCelsius()
